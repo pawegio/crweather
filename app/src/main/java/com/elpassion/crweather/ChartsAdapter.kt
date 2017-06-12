3092d8281e4c9c8ea2e8426321e5f05d5bfdf98c
Paweł Gajda <pawegio@gmail.com>
2017-06-12 16:45:07 +0200

    [Refactoring] Remove empty primary constructor

--- app/src/main/java/com/elpassion/crweather/ChartsAdapter.kt
+++ app/src/main/java/com/elpassion/crweather/ChartsAdapter.kt
@@ -5,8 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.chart.view.*
 
-
-class ChartsAdapter() : RecyclerView.Adapter<ChartsAdapter.Holder>() {
+class ChartsAdapter : RecyclerView.Adapter<ChartsAdapter.Holder>() {
 
     var charts = emptyList<Chart>()
         set(value) {
